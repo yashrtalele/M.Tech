7c7e718f9fe966240c075d6951eed27453ab9e88
Yash <yashatsocials@gmail.com>
2023-10-06 21:52:45 +0530

    added 33

---
+++ first_sem/software_systems/Hands On List - 2/33/client.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+
+void main() {
+    int client_socket;
+    struct sockaddr_in server_address;
+    char message[1024];
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(8080);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
+        perror("connect");
+        exit(EXIT_FAILURE);
+    }
+    if (send(client_socket, "Hello, This is Client!\n", 23, 0) == -1) {
+        perror("send");
+        exit(EXIT_FAILURE);
+    }
+    if (recv(client_socket, message, sizeof(message), 0) == -1) {
+        perror("recv");
+        exit(EXIT_FAILURE);
+    }
+    printf("Received message from server: %s\n", message);
+    close(client_socket);
+}
---
+++ first_sem/software_systems/Hands On List - 2/33/server.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+
+void main() {
+    int server_socket, client_socket;
+    struct sockaddr_in server_address, client_address;
+    char message[1024];
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(8080);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+    listen(server_socket, 10);
+    client_socket = accept(server_socket, NULL, NULL);
+    if (client_socket == -1) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    if (recv(client_socket, message, sizeof(message), 0) == -1) {
+        perror("recv");
+        exit(EXIT_FAILURE);
+    }
+    printf("Received message from client: %s\n", message);
+    if (send(client_socket, "Hello, This is Server!\n", 23, 0) == -1) {
+        perror("send");
+        exit(EXIT_FAILURE);
+    }
+    close(server_socket);
+    close(client_socket);
+
+}
