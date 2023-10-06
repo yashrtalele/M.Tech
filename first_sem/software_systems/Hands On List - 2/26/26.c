99742100377f99207e433bde8a1f7784cd88774d
Yash <yashatsocials@gmail.com>
2023-10-06 17:28:05 +0530

    modified 26

--- first_sem/software_systems/Hands On List - 2/26/26.c
+++ first_sem/software_systems/Hands On List - 2/26/26.c
@@ -1,6 +1,7 @@
 // Write a program to send messages to the message queue. Check $ipcs -q
 
 #include<stdio.h>
+#include<unistd.h>
 #include<sys/ipc.h>
 #include<sys/msg.h>
 
@@ -13,12 +14,12 @@ struct msg_buffer {
 void main(void) {
     key_t key;
     int msgid;
-    key = ftok(".", 11);
+    key = ftok(".", 97);
     msgid = msgget(key, 0666 | IPC_CREAT);
     msg.msg_type = 1;
-    printf("Write Data : ");
+    printf("Enter Data : ");
     fgets(msg.msg_text, MAX, stdin);
     msgsnd(msgid, &msg, sizeof(msg), 0);
-    printf("Data sent to the message queue is : %s \n", msg.msg_text);
+    printf("Data sent to the message queue : %s \n", msg.msg_text);
     execlp("ipcs", "ipcs", "-q", NULL);
 }
