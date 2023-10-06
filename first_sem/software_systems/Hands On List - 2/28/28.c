da942d59f35336207ddb6e7f50cfbf73f4f731bc
Yash <yashatsocials@gmail.com>
2023-10-06 18:22:10 +0530

    added 29

--- first_sem/software_systems/Hands On List - 2/28/28.c
+++ first_sem/software_systems/Hands On List - 2/28/28.c
@@ -7,24 +7,16 @@
 void main() {
     key_t key;
     int msqid;
-    key = ftok(".", 11);
-
+    key = ftok(".", 97);
     msqid = msgget(key, 0666 | IPC_CREAT);
     struct msqid_ds ms_info;
     msgctl(msqid, IPC_STAT, &ms_info);
     struct ipc_perm *permissions = &ms_info.msg_perm;
-
-    printf("Current permissions : %d \n", permissions->mode);
-
-    // changing mode.
+    printf("Current permission : %d\n", permissions->mode);
     permissions->mode = 777;
     ms_info.msg_perm = *permissions;
     msgctl(msqid, IPC_SET, &ms_info);
-
-    printf("\nNew permissions set\n");
-
+    printf("Changed permissions\n");
     msgctl(msqid, IPC_STAT, &ms_info);
-
-    printf("New permissions : %d \n", permissions->mode);
-    return 0;
+    printf("New permission : %d \n", permissions->mode);
 }
---
+++ first_sem/software_systems/Hands On List - 2/29/29.c
@@ -0,0 +1,23 @@
+// Write a program to remove the message queue
+
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+
+void main(void) {
+    int queue_id;
+    queue_id = msgget(IPC_PRIVATE, IPC_CREAT | 0666);
+    printf("Message id = %d\n", queue_id);
+    if (queue_id == -1) {
+        perror("msgget");
+        exit(1);
+    }
+    if(msgctl(queue_id, IPC_RMID, NULL) == -1) {
+        perror("msgctl");
+        exit(1);
+    }
+    char *args[] = {"ipcs", "-q", NULL};
+    execv("/bin/ipcs", args);
+}
