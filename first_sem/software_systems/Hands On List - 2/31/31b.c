9e3ef373563e5635ed6ea4d372d719c819aeb1d1
Yash <yashatsocials@gmail.com>
2023-10-06 20:21:58 +0530

    modified 31

--- first_sem/software_systems/Hands On List - 2/31/31b.c
+++ first_sem/software_systems/Hands On List - 2/31/31b.c
@@ -1,32 +1,20 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/shm.h>
-#include<pthread.h>
-#include<semaphore.h>
 
-sem_t sem;
-int counter=0;
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/sem.h>
 
-void *thread(void *arg) {
-    sem_wait(&sem);
-    counter+=1;
-    sem_post(&sem);
-    return NULL;
-}
+union sem {
+    int val;
+    struct semid_ds *buf;
+    unsigned short int *array;
+};
 
 void main(void) {
-    pthread_t t1, t2;
-    sem_init(&sem, 0, 2);
-    if (pthread_create(&t1, NULL, thread, NULL)) {
-        perror("pthread_create");
-        exit(EXIT_SUCCESS);
-    }
-    if (pthread_create(&t2, NULL, thread, NULL)) {
-        perror("pthread_create");
-        exit(EXIT_SUCCESS);
-    }
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
-    sem_destroy(&sem);
-    printf("counter = %d", counter);
+    union sem arg;
+    int key, semid;
+    key = ftok("file", 97);
+    semid = semget(key, 1, IPC_CREAT | 0744);
+    printf("%d\n", semid);
+    arg.val = 2;
+    semctl(semid, 0, SETVAL, arg);
 }
